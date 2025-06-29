9645e09c94980fe3c1c965010e0da3e22fad3fd0
axmanalad <alex.manalad8714@gmail.com>
2025-06-29 03:05:38 -0700

    Refactor MyGames component structure and add styles for better layout and presentation

--- src/pages/MyGames.tsx
+++ src/pages/MyGames.tsx
@@ -1,10 +1,16 @@
 import "../styles/pages/my-games.css";
 
 function MyGames() {
-  return <div className="games-empty">
-    <h2>No Games Found</h2>
-    <p>Start adding some games to your library!</p>
-  </div>
+  return (
+    <div className="my-games">
+      <div className="parent-container">
+        <div className="games-empty">
+          <h2>No Games Found</h2>
+          <p>Start adding some games to your library!</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MyGames;
